759b12ae1faf8589b32be5a1211da2202567cb31
tsf <1457990107@qq.com>
2023-09-20 21:58:29 +0800

    21:58

--- main.go
+++ main.go
@@ -1,23 +1,73 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"log"
+//
+//	"go_spark_ai/db"
+//	"go_spark_ai/service"
+//
+//	"github.com/gin-gonic/gin"
+//)
+//
+//func main() {
+//	if err := db.Init(); err != nil {
+//		panic(fmt.Sprintf("mysql init failed with %+v", err))
+//	}
+//	router := gin.Default()
+//
+//	router.POST("/message/post", service.SparkAIHandler)
+//
+//	fmt.Println("Service Listening port: 80")
+//	log.Fatal(router.Run(":80"))
+//}
+
 package main
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
-	"go_spark_ai/db"
-	"go_spark_ai/service"
+	"go_spark_ai/util"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
-	router := gin.Default()
+	r := gin.Default()
 
-	router.POST("/message/post", service.SparkAIHandler)
+	r.POST("/send", func(c *gin.Context) {
+		var reqMsg struct {
+			ToUserName   string `json:"ToUserName"`
+			FromUserName string `json:"FromUserName"`
+			MsgType      string `json:"MsgType"`
+			Content      string `json:"Content"`
+			CreateTime   int64  `json:"CreateTime"`
+		}
 
-	fmt.Println("Service Listening port: 80")
-	log.Fatal(router.Run(":80"))
+		if err := c.BindJSON(&reqMsg); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "JSON数据包解析失败"})
+			return
+		}
+
+		if reqMsg.MsgType == "text" {
+			switch reqMsg.Content {
+			case "你是":
+				//获取星火AI
+				answer := util.SparkAnswer(reqMsg.Content)
+				c.JSON(http.StatusOK, gin.H{
+					"ToUserName":   reqMsg.FromUserName,
+					"FromUserName": reqMsg.ToUserName,
+					"CreateTime":   reqMsg.CreateTime,
+					"MsgType":      "text",
+					"Content":      answer,
+				})
+			default:
+				c.String(http.StatusOK, "success")
+			}
+		} else {
+			c.String(http.StatusOK, "success")
+		}
+	})
+
+	r.Run(":80")
 }
--- service/spark_service.go
+++ service/spark_service.go
@@ -18,8 +18,18 @@ type JsonResult struct {
 	Data     interface{} `json:"data"`
 }
 
-// MsgBody 微信回复文本消息结构体
-type MsgBody struct {
+// RequestBody 微信回复文本消息结构体
+type RequestBody struct {
+	ToUserName   string
+	FromUserName string
+	CreateTime   int64
+	MsgType      string
+	Content      string
+	MsgId        string
+}
+
+// ResponseBody 微信回复文本消息结构体
+type ResponseBody struct {
 	ToUserName   string
 	FromUserName string
 	CreateTime   int64
@@ -29,31 +39,27 @@ type MsgBody struct {
 
 // IndexHandler 讯飞星火接口
 func SparkAIHandler(c *gin.Context) {
-	res := &JsonResult{}
-
-	var textMsg MsgBody
-	err := c.ShouldBindJSON(&textMsg)
+	var reqMsg RequestBody
+	err := c.ShouldBindJSON(&reqMsg)
 	if err != nil {
 		log.Printf("[消息接收] - JSON数据包解析失败: %v\n", err)
 		return
 	}
 
-	log.Printf("[消息接收] - 收到消息, 消息类型为: %s, 消息内容为: %s\n", textMsg.MsgType, textMsg.Content)
+	log.Printf("[消息接收] - 收到消息, 消息类型为: %s, 消息内容为: %s\n", reqMsg.MsgType, reqMsg.Content)
 
 	// 获取星火AI答案
-	answer := util.SparkAnswer(textMsg.Content)
+	answer := util.SparkAnswer(reqMsg.Content)
 
 	// 对接收的消息进行被动回复
-	WXMsgReply(c, textMsg.FromUserName, textMsg.ToUserName, answer)
-
-	res.Data = answer
+	WXMsgReply(c, reqMsg.FromUserName, reqMsg.ToUserName, answer)
 
 	c.String(http.StatusOK, "success") // 发送成功响应
 }
 
 // WXMsgReply 微信消息回复
 func WXMsgReply(c *gin.Context, fromUser, toUser, content string) {
-	repTextMsg := MsgBody{
+	respMsg := ResponseBody{
 		ToUserName:   toUser,
 		FromUserName: fromUser,
 		CreateTime:   time.Now().Unix(),
@@ -61,7 +67,7 @@ func WXMsgReply(c *gin.Context, fromUser, toUser, content string) {
 		Content:      content,
 	}
 
-	msg, err := json.Marshal(&repTextMsg)
+	msg, err := json.Marshal(&respMsg)
 	if err != nil {
 		log.Printf("[消息回复] - 将对象进行JSON编码出错: %v\n", err)
 		return
