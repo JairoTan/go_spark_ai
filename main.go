993105f41940776452b02388eb541908a6eded6d
tsf <1457990107@qq.com>
2023-09-20 15:15:20 +0800

    15:15

--- main.go
+++ main.go
@@ -3,22 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
-	"wxcloudrun-golang/db"
 	"wxcloudrun-golang/service"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	if err := db.Init(); err != nil {
-		panic(fmt.Sprintf("mysql init failed with %+v", err))
-	}
+	//if err := db.Init(); err != nil {
+	//	panic(fmt.Sprintf("mysql init failed with %+v", err))
+	//}
 	router := gin.Default()
 
-	router.GET("/message/post", service.SparkAIHandler)
+	router.POST("/message/post", service.SparkAIHandler)
 
 	fmt.Println("Service Listening port: 80")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Fatal(router.Run(":80"))
 }
